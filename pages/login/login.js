124bb07d4ccd23595859aff6c9b314b6d1e59cff
chirag <chirag4real123@gmail.com>
2024-03-29 03:49:46 +0530

    added captcha to login page

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,12 +1,91 @@
-const wrapper = document.querySelector('.wrapper');
-const loginLink = document.querySelector('.login-link');
-const registerLink = document.querySelector('.register-link');
+(function(){
+    const fonts = ["cursive", "sans-serif", "serif", "monospace"];
+    let captchaValue = "";
 
+    function generateCaptcha(){
+        let value = btoa(Math.random()*1000000000);
+        value = value.substring(0, 5+Math.random()*5);
+        captchaValue = value;
+    }
 
-registerLink.addEventListener('click', ()=>{
-    wrapper.classList.add("activ");
-})
+    function setCaptcha(){
+        let html = captchaValue.split("").map((char)=>{
+            const rotate = -20 + Math.trunc(Math.random() * 30);
+            const font = Math.trunc(Math.trunc()*fonts.length);
+            return `<span
+            style ="
+            transform: rotate(${rotate}deg);
+            font-family: ${fonts[font]};
+            "
+            >${char}</span>`
+        }).join("");
+        document.querySelector(".login-form .captcha .preview").innerHTML = html;
+    }
+    function initCaptcha(){
+        document.querySelector(".login-form .captcha .captcha-refresh").addEventListener("click", function(){
+            generateCaptcha();
+            setCaptcha();
+        });
+        generateCaptcha();
+        setCaptcha();
+    }
+    initCaptcha();
 
-loginLink.addEventListener('click', ()=>{
-    wrapper.classList.remove("activ");
-})
+    document.querySelector("#login-btn").addEventListener("click", function(){
+        let inputCaptchaValue = document.querySelector("#captcha-form").value;
+        if (inputCaptchaValue === captchaValue){
+            swal("", "Loggin In!", "success");
+        }
+        else{
+            swal("Invalid Captcha");
+        }
+    })
+})();
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     (function(){
+//         const fonts = ["cursive", "sans-serif", "serif", "monospace"];
+//         let captchaValue = "";
+    
+//         function generateCaptcha(){
+//             let value = btoa(Math.random()*1000000000);
+//             value = value.substring(0, 5+Math.random()*5);
+//             captchaValue = value;
+//             console.log(value)
+//         }
+    
+//         function setCaptcha(){
+//             let html = captchaValue.split("").map((char)=>{
+//                 const rotate = -20 + Math.trunc(Math.random() * 30);
+//                 const font = Math.trunc(Math.trunc()*fonts.length);
+//                 return `<span
+//                 style ="
+//                 transform: rotate(${rotate}deg);
+//                 font-family: ${fonts[font]};
+//                 "
+//                 >${char}</span>`
+//             }).join("");
+//             document.querySelector(".login-form .captcha .preview").innerHTML = html;
+//         }
+//         function initCaptcha(){
+//             document.querySelector(".login-form .captcha .captcha-refresh").addEventListener("click", function(){
+//                 generateCaptcha();
+//                 setCaptcha();
+//             });
+//             generateCaptcha();
+//             setCaptcha();
+//         }
+//         initCaptcha();
+    
+//         document.querySelector(".login-form #login-btn").addEventListener("click", function(){
+//             let inputCaptchaValue = document.querySelector(".login-form .captcha-form .captcha-input").value;
+//             if (inputCaptchaValue === captchaValue){
+//                 swal("", "Loggin In!", "success");
+//             }
+//             else{
+//                 swal("Invalid Captcha");
+//             }
+//         })
+//     })();
+//   });
+  
